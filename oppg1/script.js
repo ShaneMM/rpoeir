8e3aaaa0e9247d3bc5959b31716dfe9e9d56d722
ShaneMM <shaneskole07@gmail.com>
2024-08-21 12:53:01 +0200

    first commit

---
+++ oppg1/script.js
@@ -0,0 +1,11 @@
+const username = "shane"
+const password = "123"
+
+let myUsername = prompt("skriv ditt navn")
+let myPassword = prompt("Skriv ditt passord")
+
+if (username == myUsername && password == myPassword) {
+    console.log("velkommen")
+} else {
+    console.log("IKKE VELKOMMEN")
+}
